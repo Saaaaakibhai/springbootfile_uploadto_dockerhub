524b080c65547e5f4e6283305caf56d43a6af75c
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2025-05-26 12:13:35 +0600

    Update

---
+++ src/dockerpurpose/dockerdemo.java
@@ -0,0 +1,11 @@
+package dockerpurpose;
+
+public class dockerdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Docker It is working successfully");
+
+	}
+
+}
